99d952411628ce07327e4364acfeacba92005940
Luis Ramon Castilla <luisracastilla@gmail.com>
2025-05-30 09:28:19 +0200

    Funciona 4

--- F1/src/app/app.routes.ts
+++ F1/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { CalendarComponent } from './pages/calendar/calendar';
 import { StandingsComponent } from './pages/standings/standings';
+import { DriversComponent } from './pages/drivers/drivers';
 
 export const appRoutes: Routes = [
   { path: '', component: Home },
   { path: 'calendar', component: CalendarComponent },
   { path: 'standings', component: StandingsComponent },
+  { path: 'drivers', component: DriversComponent },
   { path: '**', redirectTo: '' }
 ];
